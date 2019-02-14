8d286a89d799b1feb8cf98e72c01f29a99257649
LuciaCardozo <lucia.t.cardozo@gmail.com>
2019-02-14 20:13:14 -0300

    ejercicio92

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
-  
+   var importe;
+   var descuento;
+   var iva;
+   var resultado;
+   importe = document.getElementById('elNombre').value;
+   descuento = document.getElementById('laLocalidad').value;
+   importe = parset(importe);
+   descuento = parset(descuento);
+   iva = parset(iva);
+   descuento= importe*0.90;
+   resultado=importe*0.90+iva;
+
+   alert ("tu compra es de "+importe+" tenes un descuento del 10% queda en "+descuento+" , mas el iva es "+resultado)
 }
