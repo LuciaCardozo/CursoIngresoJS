51b1b7bb352741b3936c1b00aa89e1e8b5c39223
LuciaCardozo <lucia.t.cardozo@gmail.com>
2019-02-14 20:56:59 -0300

    ejercicio92

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -2,16 +2,12 @@ function mostrar()
 {
    var importe;
    var descuento;
-   var iva;
    var importeFinal;
    importe = document.getElementById('elNombre').value;
-   descuento = document.getElementById('laLocalidad').value;
    importe = parset(importe);
-   descuento = parset(descuento);
-   iva = parset(iva);
    importeFinal = parseInt(importeFinal);
    descuento = importe*0.90;
-   importeFinal = importe*0.90+iva;
+   importeFinal = descuento*1.21;
 
    alert ("tu compra es de "+importe+" tenes un descuento del 10% queda en "+descuento+" , mas el iva es "+importeFinal)
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,7 +1,17 @@
 
 function mostrar()
 {
-
- alert("tenes dos mascotas, "+nombre+" y "+nombre+" que pesan "+peso1+" y "peso2);
-
+ var nombre1;
+ var nombre2
+ var peso1;
+ var peso2;
+ var resultado;
+ nombre1 = prompt("Ingrese el nombre de su primer mascota",nombre1);
+ peso1 = parsetInt(peso1);
+ peso1 =prompt ("Ingrese su peso",peso1);
+ nombre2 = prompt ("Ingrese el nombre de su segunda mascota",nombre2);
+ peso2=parsetInt(peso2);
+ peso2 = prompt ("peso2");
+ resultado=peso1+peso2;
+ alert("tenes dos mascotas, "+nombre+" y "+nombre+" que pesan "+peso1+" y "+peso2+" ambos pesan "+resultado);
 }
