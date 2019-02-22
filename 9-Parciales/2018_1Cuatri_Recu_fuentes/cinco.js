45ecfa4481c6695ec8aaa1fc46f85eba193fc50a
unknown <lucia.t.cardozo@gmail.com>
2019-02-21 21:24:38 -0300

    recu 4ok y 5incomp

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,34 @@
 function mostrar()
 {
+var tipoDePago;
+var paquetes;
+var descuento;
+var precioTotal;
+paquetes=prompt("Ingrese paquete")
+tipoDePago=prompt("Ingrese medio de pago");
+
+switch(tipoDePago)
+{
+	case "Tarjeta Visa":
+	case "Mercado Pago":
+	descuento=0.9;
+	break;
+	case "Paypal":
+	switch(paquetes)
+	case "Todo Incluido":
+	descuento=0.90
+	case 
+	descuento=0.85;
+	break;
+	case "Efectivo":
+	descuento=0.80;
+	break;
+	case "Otro medio de pago":
+	descuento=0.95;
+	break;
+}
+
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,57 @@
 function mostrar()
 {
+  var numeroUno;
+  var numeroDos;
+  var numeroTres;
+  var numeroCuatro;
+  var descuento;	
+  var resultado;
+  var resultadoFinal;
+  var precioMayor;
+  numeroUno = prompt("Ingrese el primer numero");
+  numeroDos = prompt("Ingrese el segundo numero");
+  numeroTres = prompt("Ingrese el tercer numero");
+  numeroCuatro = prompt("Ingrese el cuarto numero");
+  numeroUno=parseInt(numeroUno);
+  numeroDos=parseInt(numeroDos);
+  numeroTres=parseInt(numeroTres);
+  numeroCuatro=parseInt(numeroCuatro);
+
+
+  if(numeroUno>=numeroDos&&numeroUno>=numeroTres&&numeroUno>=numeroCuatro)
+  {
+  	precioMayor=numeroUno;
+  }
+  	else if(numeroDos>=numeroUno&&numeroDos>=numeroTres&&numeroDos>=numeroCuatro)
+  	{
+  		precioMayor=numeroDos;
+ 	 }
+ 	 else if(numeroTres>=numeroUno&&numeroTres>=numeroDos&&numeroTres>=numeroCuatro)
+  	{
+  		precioMayor=numeroTres;
+ 	 }
+  	else if(numeroCuatro>=numeroUno&&numeroCuatro>=numeroDos&&numeroCuatro>=numeroTres)
+  	{
+  		precioMayor=numeroCuatro;
+  	}
+
+  resultado=numeroUno+numeroDos+numeroTres+numeroCuatro;
+  if (resultado>100)
+  {
+  	descuento=0.90;
+  }
+  else if (resultado>50)
+  {
+  	descuento=0.95;
+  }
+  else if (resultado<50)
+  {
+  	descuento=1.15;
+  }
+  resultadoFinal=resultado*descuento;
+  alert("El resultado es "+resultadoFinal);
+
+
+
 
 }
