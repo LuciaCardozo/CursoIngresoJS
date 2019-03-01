75c6804c85a662b8fb9a62a5b717a2bcb5cfdec6
Lucia <jonathan.n.haedo@gmail.com>
2019-03-01 17:25:51 -0300

    incompleto

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,49 +1,7 @@
 function mostrar()
 {
 
-	var numero;
-	var sumaNegativo=0;
-	var sumaPositivo=0;
-	var cantidadNegativo=0;
-	var cantidadPositivo=0;
-	var cantidadCero=0;
-	var cantidadPares=0;
-	var promedioPositivo=0;
-	var promedioNegativo=0;
-	var diferencia=0;
-	var contador=0;
-	var respuesta="si";
-	var bandera;
-	bandera="es la primera";
 	
-	while(respuesta=="si")
-	{	
-		contador++;
-		numero=prompt("Ingrese un numero "+contador+" #:");	
-		numero=parseInt(numero);
-
-		respuesta=prompt("=>si, para continuar");
-
-	}
-		if (numero>0)
-		{
-			sumaPositivo=sumaPositivo+numero;
-			cantidadPositivo++;
-		}
-		else if (numero<0)
-		{
-			cantidadNegativo++;
-			sumaNegativo=sumaNegativo+numero;
-		}
-		else if(numero==0)
-			{
-				cantidadCero++;
-			}
-		else if (numero%2) 
-		{
-			cantidadPares++;
-		}
-		document.write("Suma de positivos "+sumaPositivo+"<br>");
 
 	
 	
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,45 +1,80 @@
 function mostrar()
 {
-	var marca;
-	var tamaño;//numero
-	var precio;//numero
-	var respuesta="si";
-	var contador=0;
-	var mayorPrecio;
-	var mayorPrecioMarca;
-	var acumulador=0;
-	var precioPromedio;
-	//1-sacar el mayor precio y la marca
-	while(respuesta=="si")
-	{
-		marca=prompt("Ingrese una marca de celular");
-		tamaño=prompt("Ingrese el tamaño");
-		precio=prompt("Ingrese el precio");
-		acumulador=acumulador+precio;
-		acumulador=acumulador+marca;
-
-		tamaño=parseInt(tamaño);
-		precio=parseInt(precio);
-
-		respuesta=prompt("=>si, para continuar");
-
-		if(contador==0)
-		{
-			mayorPrecio=precio;
-			mayorPrecioMarca=marca;
-			precioPromedio=precioPromedio+precio;
-			precioPromedio=acumulador/contador;
-		}
-		else if (precio>mayorPrecio)
-		{
-			mayorPrecio=precio;
-			mayorPrecioMarca=marca;
-		}
-		contador++;
+ 	var marca;
+ 	var tamaño;
+ 	var precio;
+ 	var mayorPrecio=0;
+ 	var mayorPrecioMarca;
+ 	var mayorPrecioTamaño;
+ 	var respuesta="si";
+ 	var sumaPrecio=0;
+ 	var acumulador=0;
+ 	var precioPromedio;
+ 	var cantidadDeCelulares=0;
+ 	var menorPrecio=0;
+ 	var menorPrecioMarca;
+ 	var menorPrecioTamaño;
+ 	var contador=0;
 
 
-		
-	 }
-	document.write("El precio mayor es "+mayorPrecio+" de la marca "+mayorPrecioMarca+"</br>"
-				   "El precio promedio es "+precioPromedio+"</br>");
+ 	while(respuesta=="si")
+ 	{
+ 		marca=prompt("Ingrese la marca del equipo");
+ 		tamaño=prompt("Ingrese el tamaño");
+ 		precio=prompt("Ingrese el precio");
+
+ 		tamaño=parseInt(tamaño);
+ 		precio=parseInt(precio);
+ 		while(tamaño<2)
+ 		{
+ 			tamaño=prompt("Error, ingrese el tamaño");
+ 		}
+ 		while(precio<1000 || precio>50000)
+ 		{
+ 			precio=prompt("Error, ingrese el precio");
+ 		}
+
+ 		respuesta=prompt("=>si, para continuar");
+
+ 		if(contador==0)
+ 		{
+ 			mayorPrecio=precio;
+ 			mayorPrecioMarca=marca;
+ 			mayorPrecioTamaño=tamaño;
+ 			menorPrecio=precio;
+ 			menorPrecioMarca=marca;
+ 			menorPrecioTamaño=tamaño;
+ 		}
+ 		
+ 		else if(precio>mayorPrecio)
+ 		{
+ 			mayorPrecio=precio;
+ 			mayorPrecioMarca=marca;
+ 			mayorPrecioTamaño=tamaño;
+ 		}
+ 		else if(precio<menorPrecio)
+ 		{
+ 			menorPrecio=precio;
+ 			menorPrecioMarca=marca;
+ 			menorPrecioTamaño=tamaño;
+ 		}
+ 		
+ 		if (precio>0)
+ 		{
+ 			acumulador++;
+ 			sumaPrecio=sumaPrecio+precio;
+ 		}
+ 		if(precio>2000)
+ 		{
+ 			cantidadDeCelulares++;
+ 		}
+ 		contador++;
+ 		
+ 	}
+ 	precioPromedio=sumaPrecio/acumulador;
+ 	document.write ("El precio mayor es "+mayorPrecio+" y la marca es "+mayorPrecioMarca+"</br>"+
+ 					"El promedio es "+precioPromedio+"</br>"+
+ 					"Cantidad de celulares que tiene un valor mayor a $2000.-: "+cantidadDeCelulares+"</br>"+
+ 					"El celular mas caro es "+mayorPrecioMarca+" de "+mayorPrecioTamaño+" pulgadas, con el valor de "+mayorPrecio+"</br>"+
+ 					"El celular mas barato es "+menorPrecioMarca+" de "+menorPrecioTamaño+ " pulgadas, con el valor de "+menorPrecio+"</br>");
 }
