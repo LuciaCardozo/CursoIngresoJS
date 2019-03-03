f7e607fc69bf5d04330ba43474c5f0342f94ff6f
Lucia <lucia.t.cardozo@gmail.com>
2019-03-02 23:37:02 -0300

    parcial 7 ok

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -2,39 +2,60 @@ function mostrar()
 {
  var nota;
  var sexo;
- var contador;
+ var contador=0;
  var promedio;
  var notaBaja;
- var
+ var notaAlta;
+ var sexoAlto;
+ var sexoBajo;
+ var cantidadDeVarones=0;
+ var sumaNota=0;
 
-
- contador=0;
- while(contado<5)
+ while(contador<5)
  {
- 	contador=contador+1;
- 	nota=parseInt(nota);
  	nota=prompt("Ingrese su nota del 0 al 10: ");
- 	
- 	while(nota<0 || nota>10)
- 		{
- 			nota=prompt("Error, Ingrese su nota del 0 al 10");
- 		}
+ 	nota=parseInt(nota);
+ 	{
+ 		while(nota<0 || nota>10)
+ 			{
+ 				nota=prompt("Error, Ingrese su nota del 0 al 10");
+ 			}
+ 		if(nota>0)
+ 			{
+ 				sumaNota=sumaNota+nota;
+ 			}
  	sexo=prompt("Ingrese su sexo con f o m: ");
  	while(sexo!="f" && sexo!="m")
  		{
  			sexo=prompt("Error, Ingrese su sexo con f o m");
  		}
- 		if(contador==1)
- 		{
- 			notaBaja=nota;
- 		}
-
- 	
-
- 	
-
-
-
-
+ 		if(contador==0)
+ 			{
+ 				notaBaja=nota;
+ 				sexoBajo=sexo;
+ 				notaAlta=nota;
+ 				sexoAlto=sexo
+ 			}
+ 		else if(nota<notaBaja)
+ 			{
+ 				notaBaja=nota;
+ 				sexoBajo=sexo;
+ 			}
+ 			
+ 		if(sexo=="m" || sexo=="M")
+ 			{
+ 				cantidadDeVarones++;
+ 			}
+ 			else if (nota>=6 && nota<=10)
+ 				{
+ 					notaAlta=nota;
+ 				}
+ 		contador++;
+ 	}
  }
+ promedio=sumaNota/contador;
+
+ document.write("El promedio es "+promedio+"</br>"+
+ 				"La nota mas baja es "+notaBaja+" y el sexo es "+sexoBajo+"</br>"+
+ 				"La cantidad de varones es "+cantidadDeVarones+" con nota igual o mas de 6. </br>");
 }
